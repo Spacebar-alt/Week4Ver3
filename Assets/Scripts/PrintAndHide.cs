736a4526fc1fb6bf435340ef4f95e3bfa614e03e
Pranav Mandalapu <pranavmandalapu@gmail.com>
2022-08-29 07:43:43 +1000

    90 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     // Start is called before the first frame update
     int i = 3;
+
+    public Renderer rend;
 
     void Start()
     {
